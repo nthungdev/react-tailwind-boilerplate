11cbd3690fc9bf2d85a35ab2fb5c11f64bad1cb2
Hung Nguyen <nthungdev@gmail.com>
2020-07-16 22:00:29 -0400

    Init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-blue-600 h-screen text-white flex items-center">
+      <h1 className="text-center font-bold text-5xl w-full">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
