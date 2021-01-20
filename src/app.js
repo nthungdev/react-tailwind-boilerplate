9aa5c0391a8ed066acd7702cf34defb1c886955a
Hung Nguyen <nthungdev@gmail.com>
2021-01-20 11:55:04 -0500

    Change to named export for grepability

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default () => {
+const App = () => {
   return (
     <div className="bg-blue-600 h-screen text-white flex items-center">
       <h1 className="text-center font-bold text-5xl w-full">Hello World!</h1>
     </div>
   )
 }
+
+export default App
