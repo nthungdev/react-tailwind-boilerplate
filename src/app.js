6c7df1ced3720fb251394a6cf05ba2a794dbf4de
Hung Nguyen <nthungdev@gmail.com>
2021-01-20 11:34:54 -0500

    change file name to lowercase

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const App = () => {
+export default () => {
   return (
     <div className="bg-blue-600 h-screen text-white flex items-center">
       <h1 className="text-center font-bold text-5xl w-full">Hello World!</h1>
     </div>
   )
 }
-
-export default App
